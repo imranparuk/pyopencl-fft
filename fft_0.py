478ec5f9dc492ca672661d09e1ee9e7c4f511b45
imran <parukboy@gmail.com>
2018-01-28 23:45:56 +0200

    Updated Kernel
    No build errors in kernel

--- fft_0.py
+++ fft_0.py
@@ -8,6 +8,9 @@ Created on Fri Jan 19 02:39:51 2018
 #check out this when you have time (not used yet -> for optimization)
 #http://www.bealto.com/gpu-fft_group-2.html
 
+#github pyopencl fft
+#https://github.com/pradeepsinngh/fft-dft-opencl
+
 import pyopencl as cl
 import numpy as np
 
@@ -60,41 +63,46 @@ c_buf = cl.Buffer(ctx, mf.WRITE_ONLY, c.nbytes)
 
 
 prg2 = cl.Program(ctx, """
-   __kernel void dft(
+# define len 64 //// denotes value of N point
+__kernel __attribute__ ((reqd_work_group_size(2*len,1,1)))
+void dft(
 	__global const float2 *in, // complex values input
-	__global float2 *out)                         // 1 for forward transform, -1 for backward.
+	__global float2 *out,      // complex values output
+	int length,                 // number of input and output values
+	int sign)                   // sign modifier in the exponential :
+	                            // 1 for forward transform, -1 for backward.
 {
 	// Get the varying parameter of the parallel execution :
 	int i = get_global_id(0);
-	int length = 10;
+
 	// In case we're executed "too much", check bounds :
 	if (i >= length)
 		return;
-	
-    int sign = 1;
+
 	// Initialize sum and inner arguments
 	float2 tot = 0;
-	float2 param = (-2 * sign * i) * M_PI / (float2)length;
-	
+	float param = (-2 * sign * i) * 3.141593f / (float)length;
+
 	for (int k = 0; k < length; k++) {
 		float2 value = in[k];
-		
-		// Compute sin and cos in a single call : 
-		float2 c;
-		float2 s = sincos(k * param, &c);
-		
+
+		// Compute sin and cos in a single call :
+		float c;
+		float s = sincos(k * param, &c);
+
 		// This adds (value.x * c - value.y * s, value.x * s + value.y * c) to the sum :
 		tot += (float2)(
-			dot(value, (float2)(c, -s)), dot(value, (float2)(s, c))
+			dot(value, (float2)(c, -s)),
+			dot(value, (float2)(s, c))
 		);
 	}
-	
+
 	if (sign == 1) {
 		// forward transform (space -> frequential)
 		out[i] = tot;
 	} else {
 		// backward transform (frequential -> space)
-		out[i] = tot / (float2)length;
+		out[i] = tot / (float)length;
 	}
 }""").build();
         
@@ -109,3 +117,8 @@ print("multiplied A*B:")
 print(a_mul_b.reshape(n, m))
 print("python fft:")
 print(z.reshape(n,m))
+
+
+
+
+
